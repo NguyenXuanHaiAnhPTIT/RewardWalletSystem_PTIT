5de42edc2bbef4770336959ebea034366639b18a
PhanPhúcThịnh <phanphucthinh15@gmail.com>
2025-07-05 18:40:50 +0700

    Add files via upload

---
+++ Src/Transaction.cpp
@@ -0,0 +1,43 @@
+﻿#include "../include/Transaction.h"
+#include <iostream>
+
+// Constructor mặc định
+Transaction::Transaction() :
+    transactionID(""),
+    fromUser(""),
+    toUser(""),
+    amount(0),
+    timeStamp(""),
+    status(""),
+    note("")
+{
+}
+
+// Constructor đầy đủ
+Transaction::Transaction(const std::string& transactionID,
+    const std::string& fromUser,
+    const std::string& toUser,
+    long long amount,
+    const std::string& timeStamp,
+    const std::string& status,
+    const std::string& note)
+    : transactionID(transactionID),
+    fromUser(fromUser),
+    toUser(toUser),
+    amount(amount),
+    timeStamp(timeStamp),
+    status(status),
+    note(note)
+{
+}
+
+// Hiển thị thông tin giao dịch ra console
+void Transaction::display() const {
+    std::cout << "Transaction ID : " << transactionID << std::endl;
+    std::cout << "From User      : " << fromUser << std::endl;
+    std::cout << "To User        : " << toUser << std::endl;
+    std::cout << "Amount         : " << amount << std::endl;
+    std::cout << "Time           : " << timeStamp << std::endl;
+    std::cout << "Status         : " << status << std::endl;
+    std::cout << "Note           : " << note << std::endl;
+}
---
+++ Src/Wallet.cpp
@@ -0,0 +1,21 @@
+﻿#include "../include/Wallet.h"
+#include <iostream>
+
+// Constructor mặc định
+Wallet::Wallet()
+    : walletID(""), owner(""), balance(0)
+{
+}
+
+// Constructor đầy đủ
+Wallet::Wallet(const std::string& walletID, const std::string& owner, long long balance)
+    : walletID(walletID), owner(owner), balance(balance)
+{
+}
+
+void Wallet::display() const {
+    std::cout << "----- Wallet Info -----" << std::endl;
+    std::cout << "Wallet ID : " << walletID << std::endl;
+    std::cout << "Owner     : " << owner << std::endl;
+    std::cout << "Balance   : " << balance << std::endl;
+}
